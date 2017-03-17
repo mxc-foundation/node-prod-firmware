72c07cd2559e085cf309c57e82f8b4cc1e846843
Vadim Vygonets <vadik@vygo.net>
2017-03-17 19:02:01 +0100

    proto: Better handling

--- proto.c
+++ proto.c
@@ -6,18 +6,17 @@
 #include "ble-suota.h"
 #include "sensor.h"
 
+#define DEBUG
+
 #define PORT		0x01
 
 #define CMD_MASK	0xf0
+#define CMD_SHIFT	4
 #define LEN_MASK	0x0f
 
-#define SENSOR_PERIOD	sec2osticks(10)
+#define LONG_LEN_MASK	0x3f
 
-typedef enum {
-	CMD_GET_PARAMS		= 0x00,
-	CMD_SET_PARAMS		= 0x10,
-	CMD_REBOOT_UPGRADE	= 0x20,
-} downlink_cmd;
+#define SENSOR_PERIOD	sec2osticks(10)
 
 typedef enum {
 	INFO_SENSOR_DATA	= 0x00,
@@ -26,7 +25,7 @@ typedef enum {
 
 /* CMD_REBOOT_UPGRADE */
 #define UPGRADE_BIT		0x80
-#define REBOOT_TIMEOUT_MASK	0x7f
+#define REBOOT_TIMEOUT_MASK	0x07
 
 #define ARRAY_SIZE(x)	(sizeof(x) / sizeof(*x))
 
@@ -45,16 +44,18 @@ static const ostime_t	reboot_timeouts[] = {
 	/* TBD */
 };
 
-static osjob_t	sensor_job, reboot_job;
-
 static uint8_t	pend_tx_data[MAX_LEN_PAYLOAD];
 static uint8_t	pend_tx_len;
 
 static void
 do_reboot(osjob_t *job)
 {
-	if (is_suota_ongoing()) {
-		os_setTimedCallback(job, hal_ticks() + sec2osticks(60),
+	static uint8_t	cnt;
+
+	/* If SUOTA is active, delay reboot, but force it after
+	 * 256 * 5s (21 min 20 sec). */
+	if (is_suota_ongoing() && --cnt) {
+		os_setTimedCallback(job, hal_ticks() + sec2osticks(5),
 		    do_reboot);
 		return;
 	}
@@ -62,16 +63,7 @@ do_reboot(osjob_t *job)
 }
 
 static void
-schedule_reboot(uint8_t tmout)
-{
-	if (tmout >= ARRAY_SIZE(reboot_timeouts))
-		return; // XXX
-	os_setTimedCallback(&reboot_job, hal_ticks() + reboot_timeouts[tmout],
-	    do_reboot);
-}
-
-static void
-handle_ble_on(void)
+ble_on(void)
 {
 	static OS_TASK	ble_handle;
 
@@ -82,46 +74,88 @@ handle_ble_on(void)
 	status |= STATUS_BLE_ON;
 }
 
-void
-proto_handle_cmd(uint8_t port, uint8_t *data, uint8_t len)
+static void
+handle_get_params(uint8_t *data, uint8_t len)
 {
-	uint8_t	cmd;
+}
 
+static void
+handle_set_params(uint8_t *data, uint8_t len)
+{
+}
+
+static void
+handle_reboot_upgrade(uint8_t *data, uint8_t len)
+{
+	static osjob_t	reboot_job;
+	uint8_t		tmout;
+
+	switch (len) {
+	case 0:
+		os_setCallback(&reboot_job, do_reboot);
+		break;
+	case 1:
+		tmout = data[0] & REBOOT_TIMEOUT_MASK;
+		if (tmout >= ARRAY_SIZE(reboot_timeouts))
+			break; // XXX
+		os_setTimedCallback(&reboot_job,
+		    hal_ticks() + reboot_timeouts[tmout], do_reboot);
+		if (data[0] & UPGRADE_BIT)
+			ble_on();
+		break;
+	default:
+		break;
+	}
+}
+
+typedef enum {
+	CMD_GET_PARAMS		= 0x0,
+	CMD_SET_PARAMS		= 0x1,
+	CMD_REBOOT_UPGRADE	= 0x2,
+} downlink_cmd;
+
+static void	(* const downlink_handlers[])(uint8_t *, uint8_t) = {
+	[CMD_GET_PARAMS]	= handle_get_params,
+	[CMD_SET_PARAMS]	= handle_set_params,
+	[CMD_REBOOT_UPGRADE]	= handle_reboot_upgrade,
+};
+
+static void
+proto_handle(uint8_t port, uint8_t *data, uint8_t len)
+{
+	uint8_t	cmd, plen;
+
+#ifdef DEBUG
+	printf("rx:");
+	for (int i = 0; i < len; i++)
+		printf(" %02x", data[i]);
+	printf("\r\n");
+#endif
 	if (port != PORT)
 		return;
 	while (len > 0) {
 		cmd = *data++;
 		len--;
-		if ((cmd & LEN_MASK) > len)
-			return;
-		switch ((downlink_cmd)(cmd & CMD_MASK)) {
-		case CMD_GET_PARAMS:
-			break;
-		case CMD_SET_PARAMS:
-			break;
-		case CMD_REBOOT_UPGRADE:
-			switch (cmd & LEN_MASK) {
-			case 0:
-				schedule_reboot(0);
+		plen = cmd & LEN_MASK;
+		if (plen == 0x0f) {
+			if (!len)
 				break;
-			case 1:
-				if (*data & UPGRADE_BIT)
-					handle_ble_on();
-				schedule_reboot(*data & REBOOT_TIMEOUT_MASK);
-				break;
-			default:
-				return;
-			}
-			break;
-		default:
-			return;
+			plen = *data++ & LONG_LEN_MASK;
+			len--;
 		}
-		data += cmd & LEN_MASK;
-		len -= cmd & LEN_MASK;
+		if (plen > len)
+			break;
+		cmd >>= CMD_SHIFT;
+		if (cmd < ARRAY_SIZE(downlink_handlers) &&
+		    downlink_handlers[cmd]) {
+			(*downlink_handlers[cmd])(data, plen);
+		}
+		data += plen;
+		len -= plen;
 	}
 }
 
-void
+static void
 proto_send_sensor_data(osjob_t *job)
 {
 	size_t	len;
@@ -136,15 +170,18 @@ proto_send_sensor_data(osjob_t *job)
 		return;
 	pend_tx_data[pend_tx_len] = INFO_SENSOR_DATA | (len & LEN_MASK);
 	pend_tx_len += len + 1;
+#ifdef DEBUG
 	printf("set tx data:");
 	for (int i = 0; i < pend_tx_len; i++)
 		printf(" %02x", pend_tx_data[i]);
 	printf("\r\n");
+#endif
 	LMIC_setTxData2(PORT, pend_tx_data, pend_tx_len, 0);
 	status |= STATUS_TX_PENDING;
 }
 
-void
+#ifdef DEBUG
+static void
 debug_event(int ev)
 {
 	static const char* evnames[] = {
@@ -168,10 +205,15 @@ debug_event(int ev)
 	};
 	printf("%s\r\n", (ev < sizeof(evnames)/sizeof(evnames[0])) ? evnames[ev] : "EV_UNKNOWN" );
 }
+#else
+#define debug_event(ev)
+#endif
 
 void
 onEvent(ev_t ev)
 {
+	static osjob_t	sensor_job;
+
 	debug_event(ev);
 	switch(ev) {
 	case EV_JOINED:
@@ -184,7 +226,7 @@ onEvent(ev_t ev)
 		break;
 	case EV_TXCOMPLETE:
 		if (LMIC.dataLen != 0) {
-			proto_handle_cmd(LMIC.frame[LMIC.dataBeg - 1],
+			proto_handle(LMIC.frame[LMIC.dataBeg - 1],
 			    LMIC.frame + LMIC.dataBeg, LMIC.dataLen);
 		}
 		break;
